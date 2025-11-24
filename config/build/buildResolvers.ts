36f056e56a54998e3c401e612357f0c3de703ede
equalsgood <connection.babak@gmail.com>
2025-11-24 17:41:52 +0200

    absolute imports

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
 import { ResolveOptions } from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index'],
+        alias: {},
         extensions: ['.tsx', '.ts', '.js'],
     }
 }
