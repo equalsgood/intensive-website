a28f5777981779485db18129547268ad85a0c6b1
equalsgood <connection.babak@gmail.com>
2025-12-04 23:41:08 +0200

    making useful links reusable, adding useful links to faq page

--- src/pages/HomePage/components/HomePage.tsx
+++ src/pages/HomePage/components/HomePage.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { HomeIntroduction, StudyingPrograms, Teachers, WhyWe, Testimonials } from "sections/home";
 import { TryForFree, UsefulLinks } from "sections/shared";
-import { Divider } from 'shared/components'
+import { CustomLinkProps, Divider } from 'shared/components'
+import { RoutePaths } from "app/providers/AppRouter";
+import questionMark from "shared/assets/images/question-mark.png";
+import map from "shared/assets/images/map.png";
+import contactUs from "shared/assets/images/contact-us.png";
+
+const linksConfig: Array<CustomLinkProps> = [
+    { to: RoutePaths.FAQ, img: questionMark, description: "Сторінка з відповідями на поширені запитання", title: "Залишились запитання?" },
+    { to: RoutePaths.LOCATIONS, img: map, description: "Сторінка з розташуванням наших філій", title: "Де ми знаходимось?" },
+    { to: RoutePaths.CONTACTS, img: contactUs, description: "Сторінка з контактами репетиторського центру", title: "Хочете зв'язатись?" },
+];
 
 const HomePage = () => {
     return (
@@ -13,7 +23,7 @@ const HomePage = () => {
             <Testimonials/>
             <TryForFree/>
             <Divider/>
-            <UsefulLinks/>
+            <UsefulLinks links={linksConfig}/>
         </main>
     );
 };
--- src/sections/shared/UsefulLinks/UsefulLinks.tsx
+++ src/sections/shared/UsefulLinks/UsefulLinks.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import cls from './UsefulLinks.module.css';
 import { CustomLink, CustomLinkProps, Text, TextColor, TextWeight } from "shared/components";
-import { RoutePaths } from "app/providers/AppRouter";
-import questionMark from "shared/assets/images/question-mark.png";
-import map from "shared/assets/images/map.png";
-import contactUs from "shared/assets/images/contact-us.png";
 
+interface UsefulLinksProps {
+    links: Array<CustomLinkProps>
+}
 
-const linksConfig: Array<CustomLinkProps> = [
-    { to: RoutePaths.FAQ, img: questionMark, description: "Сторінка з відповідями на поширені запитання", title: "Залишились запитання?" },
-    { to: RoutePaths.LOCATIONS, img: map, description: "Сторінка з розташуванням наших філій", title: "Де ми знаходимось?" },
-    { to: RoutePaths.CONTACTS, img: contactUs, description: "Сторінка з контактами репетиторського центру", title: "Хочете зв'язатись?" },
-];
-
-export const UsefulLinks = () => {
+export const UsefulLinks = ({links}: UsefulLinksProps) => {
     return (
         <section className={cls.section}>
             <Text tag="h2" classNamesProps={cls.title} weight={TextWeight.EXTRA_BOLD} color={TextColor.MAIN}>
                 Корисні посилання
             </Text>
             <nav className={cls.links}>
-                { linksConfig.map(({to, title, description, img}) =>
+                { links.map(({to, title, description, img}) =>
                     <CustomLink
                         key={title}
                         to={to}
