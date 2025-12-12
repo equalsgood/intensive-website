ecfd692dd74734040065d65535a8b8b99e107bf1
equalsgood <connection.babak@gmail.com>
2025-12-12 02:48:09 +0200

    form text area, changing form on submit

--- src/sections/shared/TryForFree/TryForFree.tsx
+++ src/sections/shared/TryForFree/TryForFree.tsx
@@ -6,6 +6,7 @@ import { Step, StepProps } from "./components/Step";
 import step1 from 'shared/assets/images/steps/step1.png';
 import step2 from 'shared/assets/images/steps/step2.png';
 import step3 from 'shared/assets/images/steps/step3.png';
+import CheckIcon from 'shared/assets/icons/check.svg';
 import { E164Number } from "libphonenumber-js";
 import { isPossiblePhoneNumber, parsePhoneNumber } from 'react-phone-number-input'
 
@@ -16,12 +17,14 @@ const stepsConfig: Array<StepProps> = [
 ]
 
 export const TryForFree = () => {
+    const [submitted, setSubmitted] = useState(true);
     const [name, setName] = useState('');
     const [isNameValid, setIsNameValid] = useState(undefined);
     const [phone, setPhone] = useState<E164Number>();
     const [isPhoneValid, setIsPhoneValid] = useState(undefined);
     const [email, setEmail] = useState('');
     const [isEmailValid, setIsEmailValid] = useState(undefined);
+    const [area, setArea] = useState('');
 
     const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
@@ -47,6 +50,11 @@ export const TryForFree = () => {
         setEmail(value);
     }
 
+    const onAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        const { value } = e.target;
+        setArea(value);
+    }
+
     const submitHandler = (e: FormEvent) => {
         e.preventDefault();
         if(name.length < 3) {
@@ -57,26 +65,64 @@ export const TryForFree = () => {
         if(!isNameValid || !isPhoneValid || !isEmailValid)
             return;
 
-        console.log(name, phone, email);
+        console.log(name, phone, email, area);
+        setSubmitted(true);
+        setName('');
+        setIsNameValid(undefined);
+        setPhone('' as E164Number);
+        setIsPhoneValid(undefined);
+        setEmail('');
+        setIsEmailValid(undefined);
+        setArea('');
     }
 
     return (
         <section className={cls.section}>
             <Text tag="h2" color={TextColor.MAIN} weight={TextWeight.EXTRA_BOLD} classNamesProps={cls.title}>Спробуйте безкоштовно!</Text>
             <div className={cls.content}>
-                <form onSubmit={(e) => submitHandler(e)} className={cls.formContainer}>
-                    <Input label="Ім'я" id="input-name" type="text" value={name} onChange={onNameChange} isValid={isNameValid} invalidMessage="Введіть ім'я" />
-                    <PhoneNumberInput value={phone} onChange={onPhoneChange} isValid={isPhoneValid}/>
-                    <Input label="Електронна пошта" id="input-email" type="email" value={email} onChange={onEmailChange} isValid={isEmailValid} invalidMessage="Потрібно вказати правильний email" />
-                    <Button type="submit" variant={ButtonVariants.ACTION_SECOND} classNamesProps={cls.button} text="Залишити заявку" />
-                </form>
+                {!submitted
+                    ? <form onSubmit={(e) => submitHandler(e)} className={cls.formContainer}>
+                        <Input label="Ім'я" id="input-name" type="text" value={name} onChange={onNameChange}
+                               isValid={isNameValid} invalidMessage="Введіть ім'я"/>
+                        <PhoneNumberInput value={phone} onChange={onPhoneChange} isValid={isPhoneValid}/>
+                        <Input label="Електронна пошта" id="input-email" type="email" value={email}
+                               onChange={onEmailChange} isValid={isEmailValid}
+                               invalidMessage="Потрібно вказати правильний email"/>
+                        <Input
+                            label="Додаткова інфомація (необов'язково)"
+                            textArea
+                            id="text-area-additional"
+                            type="area"
+                            placeholder="У цьому полі Ви можете вказати будь-яку інформацію, наприклад: вік учня, рівень знань, бажаний розклад, мета навчання тощо"
+                            value={area}
+                            onChange={onAreaChange}
+                            rows={3}
+                            maxLength={200}
+                        />
+                        <Button type="submit" variant={ButtonVariants.ACTION_SECOND} classNamesProps={cls.button}
+                                text="Залишити заявку"/>
+                    </form>
+                    : <div className={cls.submitted}>
+                        <div>
+                            <Text tag="h3" color={TextColor.REVERSED} weight={TextWeight.BOLD}>
+                                Дякуємо за інформацію!
+                            </Text>
+                            <Text tag="p" color={TextColor.REVERSED} weight={TextWeight.MEDIUM}>
+                                Ми зв'яжемось з Вами найближчим часом
+                            </Text>
+                        </div>
+                        <CheckIcon className={cls.check}/>
+                        <Button type="button" variant={ButtonVariants.ACTION_SECOND} classNamesProps={cls.button}
+                                text="Повернутись" onClick={() => setSubmitted(false)}/>
+                    </div>
+                }
                 <div className={cls.descriptionContainer}>
                     <Text tag="h3" weight={TextWeight.BOLD} color={TextColor.MAIN} classNamesProps={cls.subtitle}>
                         Ми зв'яжемось з Вами протягом дня
                     </Text>
                     <ul className={cls.steps}>
-                        { stepsConfig.map(({img, text}) =>
-                            <Step key={text} img={img} text={text} />
+                        {stepsConfig.map(({img, text}) =>
+                            <Step key={text} img={img} text={text}/>
                         )}
                     </ul>
                 </div>
--- src/shared/components/Input/Input.tsx
+++ src/shared/components/Input/Input.tsx
@@ -6,17 +6,37 @@ import { Text, TextWeight } from "shared/components";
 interface InputProps {
     label: string;
     id: string;
-    type: 'text' | 'email';
+    type: 'text' | 'email' | 'area';
     value: string;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    onChange: (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => void;
     isValid?: boolean;
     invalidMessage?: string;
+    textArea?: boolean;
+    placeholder?: string;
+    rows?: number;
+    maxLength?: number;
 }
 
 export const Input = (props: InputProps) => {
-    const { label, id, type, value, onChange, isValid, invalidMessage } = props;
+    const { label, placeholder, maxLength, rows, textArea, id, type, value, onChange, isValid, invalidMessage } = props;
 
     const classes = classNames(cls.container, {[cls.invalid]: isValid === false});
+
+    if(textArea) {
+        return (
+            <div className={classes}>
+                <label htmlFor={id} className={cls.label}>{label}</label>
+                <textarea
+                    className={cls.area}
+                    value={value}
+                    onChange={e => onChange(e)}
+                    placeholder={placeholder || ''}
+                    rows={rows}
+                    maxLength={maxLength}
+                />
+            </div>
+        )
+    }
 
     return (
         <div className={classes}>
