4115a6b60f0d1844d8b3f08ac865ebe73172a003
equalsgood <connection.babak@gmail.com>
2025-12-27 05:15:55 +0200

    faq page responsive design

--- client/src/sections/faq/QuestionAnswers/components/SingleQA.tsx
+++ client/src/sections/faq/QuestionAnswers/components/SingleQA.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import cls from './SingleQA.module.css';
 import classNames from "classnames";
 import { Text, TextColor, TextWeight } from "shared/components";
@@ -7,14 +7,44 @@ import ChevronRight from "shared/assets/icons/chevron-right.svg";
 export interface SingleQAProps {
     qw: string;
     an: string;
+    isMobile?: boolean;
 }
 
-export const SingleQA = ({ qw, an }: SingleQAProps) => {
+export const SingleQA = ({ qw, an, isMobile }: SingleQAProps) => {
+    const container = useRef<HTMLDivElement>(null);
+    const qwContainer = useRef<HTMLDivElement>(null);
     const [opened, setOpened] = useState(false);
+    const [maxHeight, setMaxHeight] = useState(isMobile ? 80 : 100);
+
+    const openHandler = () => {
+        const closedHeight = qwContainer.current.clientHeight;
+        const openedHeight = container.current.scrollHeight;
+        setOpened(prev => {
+            if(prev)
+                setMaxHeight(closedHeight);
+            else
+                setMaxHeight(openedHeight);
+            return !prev;
+        })
+    }
+
+    useEffect(() => {
+        const closedHeight = qwContainer.current.clientHeight;
+        const openedHeight = container.current.scrollHeight;
+        if(!opened)
+            setMaxHeight(closedHeight);
+        else
+            setMaxHeight(openedHeight);
+    }, [isMobile]);
 
     return (
-        <div className={classNames(cls.container, {[cls.opened]: opened})} onClick={() => setOpened(prev => !prev)}>
-            <div className={cls.questionContainer}>
+        <div
+            ref={container}
+            className={classNames(cls.container, {[cls.opened]: opened})}
+            onClick={() => openHandler()}
+            style={{maxHeight: maxHeight}}
+        >
+            <div ref={qwContainer} className={cls.questionContainer}>
                 <Text tag="h4" color={TextColor.MAIN} weight={TextWeight.BOLD}>{qw}</Text>
                 <ChevronRight/>
             </div>
